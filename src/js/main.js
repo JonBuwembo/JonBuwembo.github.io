d763847cbadf0c647f51031639d9ac02ff2ebdbc
JonBuwembo <jonabuwe@outlook.com>
2026-04-10 17:55:21 -0400

    Initial Setup, Layout Implemented UI/UX Design Implemented

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+
+
+document.getElementById("github").addEventListener("click", function () {
+    window.open("https://github.com/JonBuwembo?tab=repositories", "_blank");
+})
+
+document.getElementById("linkedin").addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/jonathan-buwembo/", "_blank");
+})
+
+//Typing animation
+const text = "JONATHAN BUWEMBO";
+let i = 0;
+
+function typeEffect() {
+    if (i < text.length) {
+        document.getElementById("name").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typeEffect, 120);
+    }
+}
+
+typeEffect();
+
